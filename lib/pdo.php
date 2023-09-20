d51c1e4de0256977b19640383325fca59a9b5a7f
Julien Varachas <varachasjulien@gmail.com>
2023-09-20 18:25:46 +0200

    modif pdo

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,7 +1,6 @@
 <?php
 try {
-/**$pdo = new PDO('mysql:host=localhost; dbname=garage_parrot','root', '' );*/
-$pdo = new PDO ('mysql://cb9ybbmfjh9yoaza:mqek1m0d7wnmdi51@clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/mdrfukoudk4r1w3o');
+$pdo = new PDO('mysql:host=localhost; dbname=garage_parrot','root', '' );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } 
