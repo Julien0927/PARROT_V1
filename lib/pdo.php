798a769bac923db532dc52e82dc7bffc37874630
Julien0927 <126099600+Julien0927@users.noreply.github.com>
2024-04-18 14:51:17 +0200

    Update pdo.php

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,7 +1,5 @@
 <?php
 try {
-/*$pdo = new PDO ('mysql:host=localhost; dbname=julienvarachas_garage-parrot; port=3306; charset=utf8mb4', 'root', '');*/
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } 
 catch (PDOException $e){
